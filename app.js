5b2cbb2ea85a45e9f5bb042ee5573eb1bf3b0c0d
Justin Sterling <justinsterling44@gmail.com>
2023-10-19 05:15:55 -0400

    Updated layout and frame, added animations

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+// Adds and removes 'show' and 'hidden' classes to achieve
+// animation effect through css transitions and transfomations
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -5,10 +8,19 @@ const observer = new IntersectionObserver((entries) => {
         } else {
             entry.target.classList.remove('show');
         }
-    })
-})
+    });
+});
 const hiddenEl = document.querySelectorAll('.hidden');
+hiddenEl.forEach((el) => observer.observe(el));
 
+// Shows contact links after contact button is clicked
+var contactButton = document.getElementById("contact-button");
+contactButton.onclick = function() {
+    document.getElementById("contact-list").style.display = "inline";
+    contactButton.style.display = "none";
+}
+
+// Script for sticky header
 window.onscroll = function() {myFunction()};
         
 var header = document.getElementById("header-nav");
