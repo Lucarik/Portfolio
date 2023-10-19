f87bcc2e50261cc8e0cb234e2ec92d176bb63b4e
Justin Sterling <justinsterling44@gmail.com>
2023-10-18 20:32:07 -0400

    Added title and nav menu

---
+++ app.js
@@ -0,0 +1,23 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    })
+})
+const hiddenEl = document.querySelectorAll('.hidden');
+
+window.onscroll = function() {myFunction()};
+        
+var header = document.getElementById("header-nav");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+    } else {
+    header.classList.remove("sticky");
+    }
+}
